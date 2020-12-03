21603f472362ac4b9acff4b7b186174e77028c2b
Sam Ayler <sayler8701@gmail.com>
2020-12-03 09:59:44 -0800

    adding questions for index.js

--- index.js
+++ index.js
@@ -3,20 +3,63 @@ const inquirer = require("inquirer");
 const util = require("util");
 const fs = require("fs");
 const writeFileAsync = util.promisify(fs.writeFile);
+const genrateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+	{
+		type: "input",
+		name: "title",
+		message: "What is the title of your project",
+	},
+	{
+		type: "input",
+		name: "desciption",
+		message: "Please enter a description for your project",
+	},
+	{
+		type: "input",
+		name: "installation",
+		message: "What are the installation instructions for your projece",
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: "Enter usage information",
+	},
+	{
+		type: "input",
+		name: "contribution",
+		message: "Please enter contribution guidelines",
+	},
+	{
+		type: "input",
+		name: "testInstruction",
+		message: "Please enter test instructions",
+	},
+	{
+		type: "list",
+		name: "license",
+		message: "Pick a license",
+		choices: ["GNU", "Mozilla", "Apache", "MIT"],
+	},
+	{
+		type: "input",
+		name: "username",
+		message: "What is your GitHub username",
+	},
+	{
+		type: "input",
+		name: "email",
+		message: "What is your email address",
+	},
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
