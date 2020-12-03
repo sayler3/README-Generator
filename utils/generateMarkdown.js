a075598564654f9013f5b9088e7e89c40ad027ea
Sam Ayler <sayler8701@gmail.com>
2020-12-03 15:18:03 -0800

    adding switch statement for license data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+    // switch case for type of license used
+    switch(data.license){
+        case "GNU":
+            liBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/GPL-3.0)";
+            liInfo = "[GNU General Public License version 3](https://opensource.org/licenses/GPL-3.0)";
+    }
 	return `# ${data.title}
     
     `;
