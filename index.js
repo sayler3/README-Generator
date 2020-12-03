d8968cb849b6fe2f84c2a357ab4fa067b031df30
Sam Ayler <sayler8701@gmail.com>
2020-12-03 09:21:28 -0800

    adding dependencies to index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Declaring dependencies
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
 const questions = [
 
