e4425e433b735a261b4f94c15a6685e808038cfc
Sam Ayler <sayler8701@gmail.com>
2020-12-04 12:09:27 -0800

    adding validate function for error handling if user does not answer some questions within the questions object

--- index.js
+++ index.js
@@ -10,56 +10,57 @@ const questions = [
 	{
 		type: "input",
 		name: "title",
-        message: "What is the title of your project",
+		message: "What is the title of your project",
+		validate: valInput,
 	},
 	{
 		type: "input",
 		name: "description",
-        message: "Please enter a description for your project",
-        
+		message: "Please enter a description for your project",
+		validate: valInput,
 	},
 	{
 		type: "input",
 		name: "installation",
-        message: "What are the installation instructions for your project",
-        default: "N/A",
+		message: "What are the installation instructions for your project",
+		default: "N/A",
 	},
 	{
 		type: "input",
 		name: "usage",
-        message: "Enter usage information",
-        
+		message: "Enter usage information",
+		validate: valInput,
 	},
 	{
 		type: "input",
 		name: "contribution",
-        message: "Please enter contribution guidelines",
-        
+		message: "Please enter contribution guidelines",
+		validate: valInput,
 	},
 	{
 		type: "input",
 		name: "testInstruction",
-        message: "Please enter test instructions",
-        default: "N/A",
-    },
+		message: "Please enter test instructions",
+		default: "N/A",
+	},
 	{
 		type: "list",
 		name: "license",
 		message: "Pick a license",
-        choices: ["GNU", "Mozilla", "Apache", "MIT"],
-        default: "MIT",
+		choices: ["GNU", "Mozilla", "Apache", "MIT"],
+		default: "MIT",
 	},
 	{
 		type: "input",
 		name: "username",
-        message: "What is your GitHub username",
-        default: "sayler3",
+		message: "What is your GitHub username",
+		default: "sayler3",
 	},
 	{
 		type: "input",
 		name: "email",
-        message: "What is your email address",
-        default: "s.ayler@me.com",
+		message: "What is your email address",
+		default: "s.ayler@me.com",
 	},
 ];
 
@@ -74,18 +75,27 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 async function init() {
-    try{
-        // Prompt for questions
-        const userInput = await inquirer.prompt(questions);
-        // Pass response to generateMarkdown function
-        const markdownData = genrateMarkdown(userInput);
-        // give fileName a value
-        let fileName = "README.md";
-        // write file after reciving data
-        await writeToFile(fileName, markdownData);
-    } catch (err) {
-        console.log(err);
-    }
+	try {
+		// Prompt for questions
+		const userInput = await inquirer.prompt(questions);
+		// Pass response to generateMarkdown function
+		const markdownData = genrateMarkdown(userInput);
+		// give fileName a value
+		let fileName = "README.md";
+		// write file after reciving data
+		await writeToFile(fileName, markdownData);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+// funtion to validate if the user had answered the question
+function valInput(input) {
+	if (input.trim() === "") {
+		console.log("Please add some text to answer the question");
+	} else {
+		return true;
+	}
 }
 
 // function call to initialize program
